2ed1f2bdb38bec82f557188e5e1e72b2934bcb32
Impulse Imp <imulses4mini@gmail.com>
2023-04-01 14:36:01 +0300

    Router

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,40 +1,11 @@
-import { useState } from 'react';
-import Categories from './components/Categories/Categories';
+import Home from './routes/home/home.component';
 
-const obj = [
-  {
-    id: 1,
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-  },
-  {
-    id: 2,
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-  },
-  {
-    id: 3,
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-  },
-  {
-    id: 4,
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-  },
-  {
-    id: 5,
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-  },
-];
-
-function App() {
+const App = () => {
   return (
     <div className='App'>
-      <Categories category={obj} />
+      <Home />
     </div>
   );
-}
+};
 
 export default App;
--- e-commerce/src/main.jsx
+++ e-commerce/src/main.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+// <RouterProvider router={router} />
